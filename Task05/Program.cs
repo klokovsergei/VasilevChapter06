b98f5c6697f828c6f54b98b8cd007cc46c89e07a
Sergei Klokov <s2584835@ya.ru>
2024-02-25 13:34:09 +0300

    feat: solve task05

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -6,3 +6,49 @@
 // Предусмотрите в классе конструк­тор, который работает по тому же принципу, что и метод для присваи­вания
 // значения полю. Также в классе должен быть метод, позволяющий проверить значение поля.
 
+namespace Task05
+{
+    class ValueTask
+    {
+        private int code;
+
+        public ValueTask()
+        {
+            this.code = 0;
+        }
+
+        public ValueTask(int number)
+        {
+            this.code = (number <= 100) ? number : 100;
+        }
+
+        public void SetValue()
+        {
+            this.code = 0;
+        }
+        
+        public void SetValue(int number)
+        {
+            this.code = (number <= 100) ? number : 100;
+        }
+        public void ShowValue()
+        {
+            Console.WriteLine("Приватное поле code равно: " + this.code);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValueTask A = new ValueTask();
+            A.ShowValue();
+            ValueTask B = new ValueTask(1000);
+            B.ShowValue();
+            ValueTask C = new ValueTask(35);
+            C.ShowValue();
+            C.SetValue();
+            C.ShowValue();
+        }
+    }
+}
